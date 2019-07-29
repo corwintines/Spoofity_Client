eadabadfde4d1abf9d6c87b3712fe0a71c517ae1
Corwin Smith <corwin.smith@uleth.ca>
2019-07-28 23:28:29 -0600

    fix: process environment labels. Changed bad to window replace until we get auth route working

--- src/components/Landing/Landing.tsx
+++ src/components/Landing/Landing.tsx
@@ -26,7 +26,7 @@ const Landing = withRouter((props) => {
           <Button
             label={'Host'}
             onClick={() => {
-              props.history.push(`${process.env.REACT_SERVER_URL}/spotify/authorize`);
+              window.location.replace(`${process.env.REACT_APP_SERVER_URL}/spotify/authorize`);
             }}
           />
         </div>
