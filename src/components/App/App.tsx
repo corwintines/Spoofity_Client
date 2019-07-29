107d759dbddc5dd643b2beaa15ad0b777ca36346
Tyler Stewart <hello@tylerstewart.ca>
2019-07-28 21:31:12 -0600

    refactor: remove query params

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,6 @@
 // Libraries
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-<<<<<<< HEAD
-import { QueryParamProvider } from 'use-query-params';
-=======
->>>>>>> 890c477... WIP: router
 
 // Components
 import Landing from '../Landing/Landing'
@@ -19,19 +15,10 @@ const App: React.FC = () => {
       <div className="App">
         {/* TODO: Add logo here */}
 
-<<<<<<< HEAD
-        <QueryParamProvider ReactRouterRoute={Route}>
-          <Switch>
-            <Route exact path="/" component={Landing} />
-            <Route exact path="/:playlistCode" component={SongSelection} />
-          </Switch>
-        </QueryParamProvider>
-=======
         <Switch>
           <Route exact path="/" component={Landing} />
           <Route exact path="/:playlistCode" component={SongSelection} />
         </Switch>
->>>>>>> 890c477... WIP: router
       </div>
     </Router>
   );
